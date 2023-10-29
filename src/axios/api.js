018cb4af1bfd8fff73f5e4ffac358171d50ab7c4
prince-kumar1504 <prince15raja04@gmail.com>
2023-10-29 07:26:43 +0530

    devLink

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,13 +1,13 @@
  // for local machine
 
- const API ={
-    BASE_URL : "http://localhost:8080"
- }
+//  const API ={
+//     BASE_URL : "http://localhost:8080"
+//  }
 
  // for dev env
 
-// const API ={
-//     BASE_URL : "https://gray-alive-bluefish.cyclic.app"
-// }
+const API ={
+    BASE_URL : "https://gray-alive-bluefish.cyclic.app"
+}
 
 export default API 
