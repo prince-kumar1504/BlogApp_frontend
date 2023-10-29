f527b56fd6e7de1595381b886954efe873e6aa07
prince-kumar1504 <prince15raja04@gmail.com>
2023-10-29 23:26:55 +0530

    testing

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,15 +1,15 @@
  // for local machine
 
-//  const API ={
-//     BLOG_URL : "http://localhost:8080/BlogApp_frontend/api/v1/blog",
-//     USER_URL :"http://localhost:8080/BlogApp_frontend/api/v1/user"
-//  }
+ const API ={
+    BLOG_URL : "http://localhost:8080/api/v1/blog",
+    USER_URL :"http://localhost:8080/api/v1/user"
+ }
 
  // for dev env
 
-const API ={
-    BLOG_URL : "https://gray-alive-bluefish.cyclic.app/BlogApp_frontend/api/v1/blog",
-    USER_URL : "https://gray-alive-bluefish.cyclic.app/BlogApp_frontend/api/v1/user"  
-}
+// const API ={
+//     BLOG_URL : "https://gray-alive-bluefish.cyclic.app/api/v1/blog",
+//     USER_URL : "https://gray-alive-bluefish.cyclic.app/api/v1/user"  
+// }
 
 export default API 
